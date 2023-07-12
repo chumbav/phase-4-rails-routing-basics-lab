752012654c1ae2c8675e6bbf4140418ad5ebe8a4
Victor <victor.chumba@student.moringaschool.com>
2023-07-13 01:58:54 +0300

    completed lab

---
+++ app/controllers/student_controller.rb
@@ -0,0 +1,17 @@
+class StudentController < ApplicationController
+
+    def index
+     students =  Student.all
+     render json: students
+    end
+
+    def grades
+      students  = Student.order(grade: :desc)
+      render json: students
+    end
+
+    def highest_grade
+       students = Student.order(grade: :desc).first
+       render json: students
+    end
+end
